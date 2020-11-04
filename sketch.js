65d1339ce72dcf136db9a61b110d0f8d5cbb25a5
Kathryn-z <71240245+Kathryn-z@users.noreply.github.com>
2020-11-04 10:53:34 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,27 +23,36 @@ function preload() {
 }
 
 function setup() {
-  mode = 0;
-  createCanvas(800, 400);
-  restartGame();
-  var button = createButton("Restart");
-  button.mousePressed(restartGame);
-  GameOver();
-  instruction = createP('Speed mode:');
-  slider = createSlider(1, 5, 3);
+  createCanvas(800,400);
   for (let i = 0; i < 200; i++) {
     if (random() < 0.7) {
       packages[i] = new Package1(150 * i);
     } else packages[i] = new Package2(150 * i);
   }
+  // let button = createButton("Restart");
+  //   button.mousePressed(restartGame);
+  instruction = createP('Speed mode:');
+  slider = createSlider(1,5,3);
 }
 
 function draw() {
+  background(bg);
+  for (let Package of packages) {
+    Package.display();
+    Package.move();
+  }
+  image(character, mouseX - 57, mouseY - 15, character.width * 0.15, character.height * 0.15);
+
+  fill(225);
+  textSize(35);
+  text(clicks, 30, 35)
+
   if (mode == 0) {
+
     push();
     colorMode(RGB);
-    fill(200, 10, 200);
-    rect(0, 0, 800, 400);
+    fill(200,10,200);
+    rect(0, 0, 800, 400); 
     pop();
 
     push();
@@ -54,14 +63,11 @@ function draw() {
     pop();
 
     push();
-    fill(225);
     textSize(20);
     textAlign(CENTER)
     text('Tip: Dark Packages = 1pt, Light Packages = 2pts', 400, 325);
-    pop();
 
     push();
-    fill(225);
     textAlign(CENTER);
     textSize(25);
     textLeading(30);
@@ -70,69 +76,55 @@ function draw() {
 
     song.stop();
   }
+
   if (mode == 1) {
-    background(bg);
-    for (let Package of packages) {
-      Package.display();
-      Package.move();
-    }
-    image(character, mouseX - 57, mouseY - 15, character.width * 0.15, character.height * 0.15);
-
-    fill(225);
-    textSize(35);
-    text(clicks, 30, 35)
-
-    if (frameCount % 60 == 0 && timer > 0) {
+  if (frameCount % 60 == 0 && timer > 0) {
       timer--;
     }
-    if (timer == 0) {
-      GameOver();
-    }
+
+  if (timer == 0) {
+    colorMode(RGB);
+    fill(200,10,200);
+    rect(0, 0, 800, 400); 
+    song.stop();
+
+    push();
+    fill(225);
+    textAlign(CENTER,CENTER)
+    text('GAME OVER',400, 150)
+    textSize(35);
+    text(timer, 750, 35);
+    pop();
+
+    push();
+    fill(225);
+    textAlign(CENTER, CENTER)
+    textSize(30);
+    text('Score: '+ clicks,400,200);
+    text('Highscore: '+ clicks,400,235);
+    text('Refresh to Restart',400,300);
+    pop();
+
+    push();
+    fill(225);
+    textSize(30);
+    text(clicks, 30, 35)
+    pop(); 
   }
 }
+}
+
 
 function keyPressed() {
-  if (keyCode === ENTER) {
-    mode = 1;
-    song.play();
+  if (keyCode===ENTER) {
+    mode=1;
+    // song.play();
   }
 }
 
-function GameOver() {
-  colorMode(RGB);
-  fill(200, 10, 200);
-  rect(0, 0, 800, 400);
-  song.stop();
-
-  push();
-  fill(225);
-  textAlign(CENTER, CENTER)
-  text('GAME OVER', 400, 150)
-  textSize(35);
-  text(timer, 750, 35);
-  pop();
-
-  push();
-  fill(225);
-  textAlign(CENTER, CENTER)
-  textSize(30);
-  text('Score: ' + clicks, 400, 200);
-  text('Highscore: ' + clicks, 400, 235);
-  text("Click the 'Restart' Button to Play Again", 400, 300);
-  pop();
-
-  push();
-  fill(225);
-  textSize(35);
-  text(clicks, 30, 35)
-  pop();
-}
-
-function restartGame() {
-  mode = 0;
-  timer = 30;
-  clicks = 0;
-}
+// function restartGame() {
+//   mode=0
+// }
 
 function mousePressed() {
   for (let Package of packages) {
@@ -146,6 +138,12 @@ class Package {
       this.x -= slider.value();
     }
   }
+  // if (mouseIsPressed === true) {
+  //   song.play();
+  // }
+  // else {
+  //   song.stop();
+  // }
 }
 
 class Package1 extends Package {
@@ -171,7 +169,7 @@ class Package1 extends Package {
     image(p, this.x, this.y, this.radius, this.radius);
     pop();
 
-    textSize(35);
+    textSize(30);
     text(timer, 750, 35);
   }
 }
@@ -187,7 +185,7 @@ class Package2 extends Package {
     var d = dist(mouseX - 25, mouseY - 20, this.x, this.y);
     if (d < this.radius / 2 == true) {
       this.radius = 0.01;
-      clicks += 2;
+      clicks+=2;
     } else {
       if (d < this.radius / 2 == false) {
         clicks += 0;
@@ -199,7 +197,7 @@ class Package2 extends Package {
     image(p2, this.x, this.y, this.radius, this.radius);
     pop();
 
-    textSize(35);
+    textSize(30);
     text(timer, 750, 35);
   }
 }
